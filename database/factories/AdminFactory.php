81926facd360f2b72fde706f7ab48da851c47249
MD. Jahid Hassan <33800850+Jahid-SWE@users.noreply.github.com>
2023-05-29 21:59:35 +0600

    Incmopletede

--- database/factories/AdminFactory.php
+++ database/factories/AdminFactory.php
@@ -27,6 +27,7 @@ class AdminFactory extends Factory
             'remember_token' => Str::random(10),
            // 'profile_photo_path' => null,
             //'current_team_id' => null,
+        
         ];
     }
 }
